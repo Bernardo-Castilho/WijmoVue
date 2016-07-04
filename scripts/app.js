a8408de4a6b8667f2a0281aa05e8e9607766454a
Bernardo Castilho <bernardo-castilho@hotmail.com>
2016-07-04 18:14:55 -0300

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,50 +1,36 @@
-// create some data for our app
-var names = 'Abraham,Andrew,Carter,Charles,Daniel,David,Edward,Gunning,Jacob,John,Josiah,Pierce,Richard,Samuel,Simon,Thomas,William'.split(','),
-    data = [],
-    count = 20,
-    startDate = wijmo.DateTime.addDays(new Date(), -count);
-for (var i = 0; i < count; i++) {
-    data.push({
-        id: i,
-        name: names[i % names.length],
-        sales: Math.random() * 1000,
-        expenses: Math.random() * 500,
-        downloads: Math.random() * 2000,
-        active: i % 2 == 0,
-        date: wijmo.DateTime.addDays(startDate, i)
-    });
-}
-data = new wijmo.collections.CollectionView(data);
-
-// define app component after the document has loaded
 onload = function () {
+
+    // app model
+    var names = 'Abraham,Andrew,Carter,Charles,Daniel,David,Edward,Gunning,Jacob,John,Josiah,Pierce,Richard,Samuel,Simon,Thomas,William'.split(','),
+        count = 20,
+        startDate = wijmo.DateTime.addDays(new Date(), -count),
+        data = [];
+    for (var i = 0; i < 20; i++) {
+        data.push({
+            id: i,
+            name: names[i % names.length],
+            sales: Math.random() * 1000,
+            expenses: Math.random() * 500,
+            downloads: Math.random() * 2000,
+            active: i % 2 == 0,
+            date: wijmo.DateTime.addDays(startDate, i)
+        });
+    }
+
+    // app view (main component)
     new Vue({
         el: '#app',
         data: {
             names: names,
-            data: data,
-            gridItemsChanged: function (s, e) {
-                console.log('grid items changed');
-            },
-            initGrid: function (s, e) {
-                console.log('grid initialized');
-            },
-            selectedIndexChanged: function (s, e) {
-                console.log('selected index changed to ' + s.selectedIndex);
-            }
+            view: new wijmo.collections.CollectionView(data)
         },
-        attached: function () {
-            // invalidate grids when the values of the current item change
-            // because we're setting them directly
-            var _this = this;
-            this.$watch(
-                function () {
-                    return JSON.stringify(_this.data.currentItem)
-                },
-                function (newVal, oldVal) {
-                    wijmo.Control.invalidateAll();
-                }
-            );
+        methods: {
+
+            // refresh the CollectionView after setting values 
+            // edit/commitItem blocks
+            refreshView: function (s, e) {
+                this.view.refresh();
+            }
         }
     });
 }
--- scripts/wijmo.vue.js
+++ scripts/wijmo.vue.js
@@ -1,237 +1,257 @@
-/*
- * Wijmo components for Vue JS
+/**
+ * Wijmo Vue interop module.
  */
-
-// FlexGrid component
+var wijmo;
+(function (wijmo) {
+    var vue;
+    (function (vue) {
+        // get an array with a control's properties and events
+        function getProps(controlClass, extraProps) {
+            var p = ['control', 'initialized']; // 'special' members
+            for (var prototype = controlClass.prototype; prototype; prototype = prototype.__proto__) {
+                for (var prop in prototype) {
+                    if (prop[0] != '_' && prop != 'constructor') {
+                        if (prop.match(/\bon[A-Z]/)) {
+                            prop = prop[2].toLowerCase() + prop.substr(3);
+                        }
+                        if (p.indexOf(prop) < 0) {
+                            p.push(prop);
+                        }
+                    }
+                }
+            }
+            if (extraProps) {
+                Array.prototype.push.apply(p, extraProps);
+            }
+            return p;
+        }
+        vue.getProps = getProps;
+        // initialize control properties from component, add watchers to keep the control in sync
+        function initialize(component, control) {
+            // hook up event handlers
+            for (var prop in component._props) {
+                if (control[prop] instanceof wijmo.Event) {
+                    var event = control[prop];
+                    // fire component event handler
+                    if (wijmo.isFunction(component[prop])) {
+                        event.addHandler(component[prop], control);
+                    }
+                    // update property 'xxx' in response to 'xxxChanged' event
+                    var m = prop.match(/(\w+)Changed/);
+                    if (m && m.length) {
+                        prop = m[1];
+                        if (control[prop] != null && component[prop] != null) {
+                            event.addHandler(_update.bind({
+                                component: component,
+                                control: control,
+                                prop: prop
+                            }));
+                        }
+                    }
+                }
+            }
+            // initialize properties (after setting up event handlers)
+            for (var prop in component._props) {
+                if (!(control[prop] instanceof wijmo.Event) && component[prop] != null) {
+                    control[prop] = component[prop];
+                    component.$watch(prop, _watch.bind({ control: control, prop: prop }));
+                }
+            }
+            // set 'control' pseudo-property so it's accessible to parent component
+            if (component.control && component.$parent) {
+                component.$parent[component.control] = control;
+            }
+            // invoke 'initialized' event
+            if (wijmo.isFunction(component.initialized)) {
+                component.initialized(control);
+            }
+            // update control property to match component changes
+            function _watch(newValue) {
+                this.control[this.prop] = newValue;
+            }
+            function _update() {
+                this.component[this.prop] = this.control[this.prop];
+            }
+        }
+        vue.initialize = initialize;
+        // copy properties from an object to another
+        function copy(dst, src) {
+            for (var key in src) {
+                if (key in dst && wijmo.isPrimitive(src[key])) {
+                    dst[key] = src[key];
+                }
+            }
+        }
+        vue.copy = copy;
+        // get the name of an object from the constructor
+        function getClassName(obj) {
+            var m = obj.constructor ? obj.constructor.toString().match(/function\s+(\w+)/) : null;
+            return (m && m.length) ? m[1] : null;
+        }
+        vue.getClassName = getClassName;
+        ;
+    })(vue = wijmo.vue || (wijmo.vue = {}));
+})(wijmo || (wijmo = {}));
+// ** wijmo.input components
+Vue.component('wj-auto-complete', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.AutoComplete),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.AutoComplete(this.$el));
+    }
+});
+Vue.component('wj-calendar', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.Calendar),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.Calendar(this.$el));
+    }
+});
+Vue.component('wj-color-picker', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.ColorPicker),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.ColorPicker(this.$el));
+    }
+});
+Vue.component('wj-combo-box', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.ComboBox),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.ComboBox(this.$el));
+    }
+});
+Vue.component('wj-input-color', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.InputColor),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.InputColor(this.$el));
+    }
+});
+Vue.component('wj-input-date', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.InputDate),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.InputDate(this.$el));
+    }
+});
+Vue.component('wj-input-date-time', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.InputDateTime),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.InputDateTime(this.$el));
+    }
+});
+Vue.component('wj-input-mask', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.InputMask),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.InputMask(this.$el));
+    }
+});
+Vue.component('wj-input-number', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.InputNumber),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.InputNumber(this.$el));
+    }
+});
+Vue.component('wj-input-time', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.InputTime),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.InputTime(this.$el));
+    }
+});
+Vue.component('wj-list-box', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.ListBox),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.ListBox(this.$el));
+    }
+});
+Vue.component('wj-multi-select', {
+    template: '<div/>',
+    props: wijmo.vue.getProps(wijmo.input.MultiSelect),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.MultiSelect(this.$el));
+    }
+});
+Vue.component('wj-popup', {
+    template: '<div><slot/></div>',
+    props: wijmo.vue.getProps(wijmo.input.Popup),
+    attached: function () {
+        wijmo.vue.initialize(this, new wijmo.input.Popup(this.$el));
+    }
+});
+// ** wijmo.grid components
 Vue.component('wj-flex-grid', {
     template: '<div><slot/></div>',
-    props: wjGetProps(wijmo.grid.FlexGrid),
+    props: wijmo.vue.getProps(wijmo.grid.FlexGrid),
     attached: function () {
         var ctl = new wijmo.grid.FlexGrid(this.$el, {
             autoGenerateColumns: this.$children.length == 0
         });
-        wjInitialize(this, ctl);
+        wijmo.vue.initialize(this, ctl);
     }
 });
 Vue.component('wj-flex-grid-column', {
     template: '<div/>',
-    props: wjGetProps(wijmo.grid.Column),
+    props: wijmo.vue.getProps(wijmo.grid.Column),
     attached: function () {
-        var grid = wijmo.Control.getControl(this.$parent.$el);
-        var col = new wijmo.grid.Column();
-        wjInitialize(this, col);
+        var grid = wijmo.Control.getControl(this.$parent.$el), col = new wijmo.grid.Column();
+        wijmo.vue.initialize(this, col);
         grid.columns.push(col);
         this.$parent.$el.removeChild(this.$el);
     }
 });
-
-// FlexChart component
+// ** wijmo.chart components
 Vue.component('wj-flex-chart', {
     template: '<div><slot/></div>',
-    props: wjGetProps(wijmo.chart.FlexChart),
+    props: wijmo.vue.getProps(wijmo.chart.FlexChart),
     attached: function () {
-        var ctl = new wijmo.chart.FlexChart(this.$el);
-        wjInitialize(this, ctl);
+        wijmo.vue.initialize(this, new wijmo.chart.FlexChart(this.$el));
     }
 });
 Vue.component('wj-flex-chart-series', {
     template: '<div/>',
-    props: wjGetProps(wijmo.chart.Series),
+    props: wijmo.vue.getProps(wijmo.chart.Series),
     attached: function () {
-        var chart = wijmo.Control.getControl(this.$parent.$el),
-            series = new wijmo.chart.Series();
-        wjInitialize(this, series);
+        var chart = wijmo.Control.getControl(this.$parent.$el), series = new wijmo.chart.Series();
+        wijmo.vue.initialize(this, series);
         chart.series.push(series);
         this.$parent.$el.removeChild(this.$el);
     }
 });
-
-// InputNumber component
-Vue.component('wj-input-number', {
-    template: '<div/>',
-    props: wjGetProps(wijmo.input.InputNumber),
-    attached: function () {
-        var ctl = new wijmo.input.InputNumber(this.$el);
-        wjInitialize(this, ctl);
-    }
-})
-
-// InputDate component
-Vue.component('wj-input-date', {
-    template: '<div/>',
-    props: wjGetProps(wijmo.input.InputDate),
-    attached: function () {
-        var ctl = new wijmo.input.InputDate(this.$el);
-        wjInitialize(this, ctl);
-    }
-})
-
-// InputTime component
-Vue.component('wj-input-time', {
-    template: '<div/>',
-    props: wjGetProps(wijmo.input.InputTime),
-    attached: function () {
-        var ctl = new wijmo.input.InputTime(this.$el);
-        wjInitialize(this, ctl);
-    }
-})
-
-// InputDateTime component
-Vue.component('wj-input-date-time', {
-    template: '<div/>',
-    props: wjGetProps(wijmo.input.InputDateTime),
-    attached: function () {
-        var ctl = new wijmo.input.InputDateTime(this.$el);
-        wjInitialize(this, ctl);
-    }
-})
-
-// Calendar component
-Vue.component('wj-calendar', {
-    template: '<div/>',
-    props: wjGetProps(wijmo.input.Calendar),
-    attached: function () {
-        var ctl = new wijmo.input.Calendar(this.$el);
-        wjInitialize(this, ctl);
-    }
-})
-
-// ComboBox component
-Vue.component('wj-combo-box', {
-    template: '<div/>',
-    props: wjGetProps(wijmo.input.ComboBox),
-    attached: function () {
-        var ctl = new wijmo.input.ComboBox(this.$el);
-        wjInitialize(this, ctl);
-    }
-})
-
-// RadialGauge and LinearGauge components
+// ** wijmo.gauge components
 Vue.component('wj-radial-gauge', {
     template: '<div><slot/></div>',
-    props: wjGetProps(wijmo.gauge.RadialGauge),
+    props: wijmo.vue.getProps(wijmo.gauge.RadialGauge),
     attached: function () {
-        var ctl = new wijmo.gauge.RadialGauge(this.$el);
-        wjInitialize(this, ctl);
+        wijmo.vue.initialize(this, new wijmo.gauge.RadialGauge(this.$el));
     }
 });
 Vue.component('wj-linear-gauge', {
     template: '<div><slot/></div>',
-    props: wjGetProps(wijmo.gauge.LinearGauge),
+    props: wijmo.vue.getProps(wijmo.gauge.LinearGauge),
     attached: function () {
-        var ctl = new wijmo.gauge.LinearGauge(this.$el);
-        wjInitialize(this, ctl);
+        wijmo.vue.initialize(this, new wijmo.gauge.LinearGauge(this.$el));
     }
 });
 Vue.component('wj-range', {
     template: '<div/>',
-    props: wjGetProps(wijmo.gauge.Range, ['wjProperty']),
+    props: wijmo.vue.getProps(wijmo.gauge.Range, ['wjProperty']),
     attached: function () {
-        var gauge = wijmo.Control.getControl(this.$parent.$el),
-            rng = gauge[this.wjProperty];
+        var gauge = wijmo.Control.getControl(this.$parent.$el), rng = gauge[this.wjProperty];
         if (rng) {
-            wjCopy(rng, this);
-        } else {
+            wijmo.vue.copy(rng, this);
+        }
+        else {
             rng = new wijmo.gauge.Range();
-            wjInitialize(this, rng);
+            wijmo.vue.initialize(this, rng);
             gauge.ranges.push(rng);
         }
         this.$parent.$el.removeChild(this.$el);
     }
 });
-
-// get an array with a control's properties and events
-function wjGetProps(controlClass, extraProps) {
-    var p = [
-        'control',      // expose control to parent component (optional)
-        'initialized'   // event that occurs after the control has been fully initialized
-    ];
-
-    // scan class hierarchy and collect all properties/events
-    for (var prototype = controlClass.prototype; prototype; prototype = prototype.__proto__) {
-        for (var prop in prototype) {
-            if (prop[0] != '_' && prop != 'constructor') {
-                if (prop.match(/\bon[A-Z]/)) {
-                    prop = prop[2].toLowerCase() + prop.substr(3);
-                }
-                if (p.indexOf(prop) < 0) {
-                    p.push(prop);
-                }
-            }
-        }
-    }
-
-    // add any extra properties
-    if (extraProps) {
-        Array.prototype.push.apply(p, extraProps);
-    }
-
-    // return the whole list
-    return p;
-}
-
-// initialize control properties from component, add watchers to keep the control in sync
-function wjInitialize(component, control) {
-
-    // hook up event handlers
-    for (var prop in component._props) {
-        if (control[prop] instanceof wijmo.Event) {
-            var event = control[prop];
-
-            // fire component event handler
-            if (wijmo.isFunction(component[prop])) {
-                event.addHandler(component[prop], control);
-            }
-
-            // update property 'xxx' in response to 'xxxChanged' event
-            var m = prop.match(/(\w+)Changed/);
-            if (m && m.length) {
-                prop = m[1];
-                if (control[prop] != null && component[prop] != null) {
-                    event.addHandler(_update.bind({
-                        component: component,
-                        control: control,
-                        prop: prop
-                    }));
-                }
-            }
-        }
-    }
-
-    // initialize properties (after setting up event handlers)
-    for (var prop in component._props) {
-        if (!(control[prop] instanceof wijmo.Event) && component[prop] != null) {
-            control[prop] = component[prop];
-            component.$watch(prop, _watch.bind({ control: control, prop: prop }));
-        }
-    }
-
-    // set 'control' pseudo-property so it's accessible to parent component
-    if (component.control && component.$parent) {
-        component.$parent[component.control] = control;
-    }
-
-    // invoke 'initialized' event
-    if (wijmo.isFunction(component.initialized)) {
-        component.initialized(control);
-    }
-
-    // update control property to match component changes
-    function _watch(newValue) {
-        this.control[this.prop] = newValue;
-    }
-
-    // update component property to match control changes
-    function _update() {
-        this.component[this.prop] = this.control[this.prop];
-    }
-}
-
-// copy properties from an object to another
-function wjCopy(dst, src) {
-    for (var key in src) {
-        if (key in dst && wijmo.isPrimitive(src[key])) {
-            dst[key] = src[key];
-        }
-    }
-}
+//# sourceMappingURL=wijmo.vue.js.map
